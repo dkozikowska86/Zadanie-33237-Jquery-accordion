99d08ce131f6bd7affba4cb07c895d3432f26473
dkozikowska86 <92026031+dkozikowska86@users.noreply.github.com>
2021-12-15 10:06:55 +0100

    after

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-console.log("Javascript");
+$(function() {
+
+    $('.accordion-item').click(function() {
+        if ($(this).hasClass('open')) {
+            $(this).removeClass('open');
+            $(this).find('.accordion-content').slideUp();
+        } else {
+            $(this).addClass('open');
+            $(this).find('.accordion-content').slideDown();
+        }
+    });
+});
